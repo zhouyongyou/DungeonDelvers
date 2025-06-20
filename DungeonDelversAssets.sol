b245e7859b8e5fff69deb8fff83a31f736bb17e9
ZhouYongyou <128128010+zhouyongyou@users.noreply.github.com>
2025-06-20 20:10:48 +0800

    Update V10.0: 為所有核心外部合約地址增加了 setter 函式，提供最大程度的部署後靈活性 DungeonDelversAssets.sol
     * @title DungeonDelversAssets (完全可配置最終版)
     * @dev V10.0: 為所有核心外部合約地址增加了 setter 函式，提供最大程度的部署後靈活性。
     * - 擁有者可以更新遊戲代幣、穩定幣、流動性池和 VRF 的相關配置。

--- DungeonDelversAssets.sol
+++ DungeonDelversAssets.sol
@@ -8,7 +8,7 @@ import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBa
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 /**
- * @dev 這是與 PancakeSwap V2 流動性池 (Pair) 合約互動所需的標準介面。
+ * @dev 與 PancakeSwap V2 流動性池 (Pair) 合約互動所需的標準介面。
  */
 interface IPancakePair {
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
@@ -16,12 +16,9 @@ interface IPancakePair {
 }
 
 /**
- * @title DungeonDelversAssets (滑價保護最終版)
- * @dev V7.0: 加入了滑價保護機制，讓用戶可以傳入願意支付的最高代幣數量，防止交易失敗或超額支付。
- * - requestNewHero 和 requestNewRelic 函式新增了 maxAmountIn 參數。
- * - 在執行轉帳前，會驗證實際所需數量是否超過用戶的限制。
- * - 價格錨定於與穩定幣組成的流動性池。
- * - 保留 Chainlink VRF V2 以確保鑄造的公平性與安全性。
+ * @title DungeonDelversAssets (完全可配置最終版)
+ * @dev V10.0: 為所有核心外部合約地址增加了 setter 函式，提供最大程度的部署後靈活性。
+ * - 擁有者可以更新遊戲代幣、穩定幣、流動性池和 VRF 的相關配置。
  */
 contract DungeonDelversAssets is ERC1155, Ownable, VRFConsumerBaseV2 {
     // --- Token ID 常數 ---
@@ -37,25 +34,35 @@ contract DungeonDelversAssets is ERC1155, Ownable, VRFConsumerBaseV2 {
     uint256 public constant EPIC_RELIC = 14;
     uint256 public constant LEGENDARY_RELIC = 15;
 
-    // --- 狀態變數 ---
-    IERC20 public immutable soulShardToken;
-    IPancakePair public immutable soulShardUsd1Pair; // $SoulShard/USD1 流動性池合約
-    address public immutable usd1Token;              // USD1 穩定幣的合約地址
-    uint256 public heroMintPriceUSD = 2 * 10**18;   // 英雄鑄造價格: 2 USD (假設 USD1 有 18 位小數)
-    uint256 public relicMintPriceUSD = 2 * 10**18;  // 聖物鑄造價格: 2 USD (假設 USD1 有 18 位小數)
+    // --- 所有核心地址變數 ---
+    IERC20 public soulShardToken;
+    IPancakePair public soulShardUsdPair;
+    address public usdToken;
+    VRFCoordinatorV2Interface public vrfCoordinator;
+    uint64 public subscriptionId;
+    bytes32 public keyHash;
+
+    // --- 其他狀態變數 ---
+    uint256 public heroMintPriceUSD = 2 * 10**18;
+    uint256 public relicMintPriceUSD = 2 * 10**18;
     uint256 private s_tokenCounter;
-    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
-    uint64 private immutable i_subscriptionId;
-    bytes32 private immutable i_keyHash;
+
+    // --- Chainlink VRF V2 常數 ---
     uint32 private constant CALLBACK_GAS_LIMIT = 250000;
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
+
+    // --- 儲存請求與 NFT 屬性 ---
     enum RequestType { Hero, Relic }
     struct RequestStatus { address requester; RequestType requestType; }
     mapping(uint256 => RequestStatus) public s_requests;
     mapping(uint256 => uint256) public nftPower;
     mapping(uint256 => uint256) public nftCapacity;
     mapping(uint256 => uint256) public nftType;
+
+    // --- 事件 ---
+    event ConfigAddressUpdated(string indexed name, address indexed newAddress);
+    event VrfConfigUpdated(uint64 newSubscriptionId, bytes32 newKeyHash);
     event MintPriceUpdated(uint256 newHeroPriceUSD, uint256 newRelicPriceUSD);
     event MintRequested(uint256 indexed requestId, address indexed requester, RequestType requestType);
     event MintFulfilled(uint256 indexed requestId, uint256 indexed tokenId, uint256 tokenType, uint256 powerOrCapacity);
@@ -74,7 +81,7 @@ contract DungeonDelversAssets is ERC1155, Ownable, VRFConsumerBaseV2 {
         address _initialOwner,
         string memory _uri,
         address _soulShardTokenAddress,
-        address _usd1TokenAddress,
+        address _usdTokenAddress,
         address _pairAddress,
         address _vrfCoordinatorV2,
         uint64 _subscriptionId,
@@ -85,22 +92,22 @@ contract DungeonDelversAssets is ERC1155, Ownable, VRFConsumerBaseV2 {
     VRFConsumerBaseV2(_vrfCoordinatorV2) 
     {
         soulShardToken = IERC20(_soulShardTokenAddress);
-        usd1Token = _usd1TokenAddress;
-        soulShardUsd1Pair = IPancakePair(_pairAddress);
-        i_vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinatorV2);
-        i_subscriptionId = _subscriptionId;
-        i_keyHash = _keyHash;
+        usdToken = _usdTokenAddress;
+        soulShardUsdPair = IPancakePair(_pairAddress);
+        vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinatorV2);
+        subscriptionId = _subscriptionId;
+        keyHash = _keyHash;
     }
 
     // --- 動態定價核心邏輯 ---
     function getSoulShardAmountForUSD(uint256 _amountUSD) public view returns (uint256) {
-        (uint reserve0, uint reserve1, ) = soulShardUsd1Pair.getReserves();
-        address token0 = soulShardUsd1Pair.token0();
-        (uint reserveSoulShard, uint reserveUSD1) = (token0 == address(soulShardToken)) 
+        (uint reserve0, uint reserve1, ) = soulShardUsdPair.getReserves();
+        address token0 = soulShardUsdPair.token0();
+        (uint reserveSoulShard, uint reserveUSD) = (token0 == address(soulShardToken)) 
             ? (reserve0, reserve1) 
             : (reserve1, reserve0);
-        require(reserveSoulShard > 0 && reserveUSD1 > 0, "無效的儲備量");
-        return ((_amountUSD * reserveSoulShard * 1000) / (reserveUSD1 * 997)) + 1;
+        require(reserveSoulShard > 0 && reserveUSD > 0, "無效的儲備量");
+        return ((_amountUSD * reserveSoulShard * 1000) / (reserveUSD * 997)) + 1;
     }
     
     // --- 外部函式 (請求鑄造) ---
@@ -120,7 +127,7 @@ contract DungeonDelversAssets is ERC1155, Ownable, VRFConsumerBaseV2 {
 
     // --- 內部核心邏輯 ---
     function _requestRandomness(RequestType _requestType) private {
-        uint256 requestId = i_vrfCoordinator.requestRandomWords(i_keyHash, i_subscriptionId, REQUEST_CONFIRMATIONS, CALLBACK_GAS_LIMIT, NUM_WORDS);
+        uint256 requestId = vrfCoordinator.requestRandomWords(keyHash, subscriptionId, REQUEST_CONFIRMATIONS, CALLBACK_GAS_LIMIT, NUM_WORDS);
         s_requests[requestId] = RequestStatus({ requester: msg.sender, requestType: _requestType });
         emit MintRequested(requestId, msg.sender, _requestType);
     }
@@ -159,10 +166,10 @@ contract DungeonDelversAssets is ERC1155, Ownable, VRFConsumerBaseV2 {
         uint256 rarityRoll = _randomNumber % 100;
         uint256 tokenTypeToMint;
         uint256 capacity;
-        if (rarityRoll < 44) { tokenTypeToMint = COMMON_RELIC; capacity = 1; }
-        else if (rarityRoll < 79) { tokenTypeToMint = UNCOMMON_RELIC; capacity = 2; }
-        else if (rarityRoll < 94) { tokenTypeToMint = RARE_RELIC; capacity = 3; }
-        else if (rarityRoll < 99) { tokenTypeToMint = EPIC_RELIC; capacity = 4; }
+        if (rarityRoll < 44) { tokenTypeToMint = COMMON_RELIC; capacity = 1; } 
+        else if (rarityRoll < 79) { tokenTypeToMint = UNCOMMON_RELIC; capacity = 2; } 
+        else if (rarityRoll < 94) { tokenTypeToMint = RARE_RELIC; capacity = 3; } 
+        else if (rarityRoll < 99) { tokenTypeToMint = EPIC_RELIC; capacity = 4; } 
         else { tokenTypeToMint = LEGENDARY_RELIC; capacity = 5; }
         s_tokenCounter++;
         uint256 newTokenId = s_tokenCounter;
@@ -182,7 +189,31 @@ contract DungeonDelversAssets is ERC1155, Ownable, VRFConsumerBaseV2 {
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
+    
+    /**
+     * @dev 更新核心代幣地址。請極度謹慎使用。
+     * @param _newSoulShardToken 新的 $SoulShard 代幣地址
+     * @param _newUsdToken 新的穩定幣地址
+     */
+    function setTokenAddresses(address _newSoulShardToken, address _newUsdToken) public onlyOwner {
+        require(_newSoulShardToken != address(0) && _newUsdToken != address(0), "地址不可為零");
+        soulShardToken = IERC20(_newSoulShardToken);
+        usdToken = _newUsdToken;
+        emit ConfigAddressUpdated("SoulShardToken", _newSoulShardToken);
+        emit ConfigAddressUpdated("UsdToken", _newUsdToken);
+    }
 
+    function setPairAddress(address _newPairAddress) public onlyOwner {
+        soulShardUsdPair = IPancakePair(_newPairAddress);
+        emit ConfigAddressUpdated("PairAddress", _newPairAddress);
+    }
+
+    function setVrfConfig(uint64 _newSubscriptionId, bytes32 _newKeyHash) public onlyOwner {
+        subscriptionId = _newSubscriptionId;
+        keyHash = _newKeyHash;
+        emit VrfConfigUpdated(_newSubscriptionId, _newKeyHash);
+    }
+    
     function withdrawTokens(address _to) public onlyOwner {
         uint256 balance = soulShardToken.balanceOf(address(this));
         require(balance > 0, "合約中無代幣可提取");
